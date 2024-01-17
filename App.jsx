bfb54890809d74952c07f6f297dfb29ebb2c9203
Elvis Rivera <therealelvisrivera@gmail.com>
2024-01-17 10:04:34 -0600

    initial set up complete

--- App.jsx
+++ App.jsx
@@ -7,8 +7,10 @@ function App() {
 
   return (
     <>
-      <div>
-     
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">{/* routes here */}</div>
       </div>
       
     </>
